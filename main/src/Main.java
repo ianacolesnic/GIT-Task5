21023c1fbc6e8c33882390656dd7ee5526183ce8
icolesnic <ianacolesnic@ymail.com>
2020-10-29 12:35:43 +0200

    added (*255) at row nr.5

--- main/src/Main.java
+++ main/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Internship");
         System.out.println(1+3);
-        System.out.println(5*9);
+        System.out.println(5*9*10-10*40+50*255);
         System.out.println(false);
 
     }
